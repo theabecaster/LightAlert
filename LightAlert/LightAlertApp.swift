295ed08565fa9711b713a07aa9f2039265e8cbfd
theabecaster <6255888+theabecaster@users.noreply.github.com>
2025-01-09 15:00:39 -0500

    Initial Commit

---
+++ LightAlert/LightAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightAlertApp.swift
+//  LightAlert
+//
+//  Created by Abraham Gonzalez on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct LightAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LightAlertTests/LightAlertTests.swift
@@ -0,0 +1,16 @@
+//
+//  LightAlertTests.swift
+//  LightAlertTests
+//
+//  Created by Abraham Gonzalez on 1/9/25.
+//
+
+import Testing
+
+struct LightAlertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
